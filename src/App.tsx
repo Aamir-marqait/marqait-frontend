ff0e5b28dafeeb8460dcb02c041b7c5942961ca5
md-anas-sabah <mdanassabah@gmail.com>
2025-08-08 01:05:13 +0530

    Fixing

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           onToggleCollapse={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
         />
         <div className="flex-1 flex flex-col">
-          <Header />
+          <Header onMenuClick={() => setIsSidebarOpen(true)} />
           <main className="flex-1 overflow-auto">
             <Routes>
               <Route path="/" element={<Dashboard />} />
